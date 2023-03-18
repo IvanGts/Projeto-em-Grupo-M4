45b6d84c1b593804bb49b328dc2016a1cd2cb94d
Jhonny515 <jonatascemporcentojesus@gmail.com>
2023-03-17 21:06:45 -0300

    Importação de assets para o carrossel

--- valdisnei/src/components/Carrossel/Carrossel.jsx
+++ valdisnei/src/components/Carrossel/Carrossel.jsx
@@ -1,17 +1,14 @@
 import Carousel from 'react-bootstrap/Carousel';
 import styles from './Carrossel.module.css';
+import nextButton from '../../assets/nextButton.svg';
+import prevButton from '../../assets/prevButton.svg';
 import './Carrossel.css';
 
 function Carrossel() {
   return (
     <Carousel 
-      prevIcon={<svg width="30" height="37" viewBox="0 0 30 37" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M0.152466 19.328L29.2506 0.92826L29.8101 36.8116L0.152466 19.328Z" fill="#728390" fill-opacity="0.6"/>
-      </svg>}
-      nextIcon={<svg width="31" height="39" viewBox="0 0 31 39" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M30.1402 19.256L0.0187246 38.1158L0.262974 0.0114925L30.1402 19.256Z" fill="#728390" fill-opacity="0.6"/>
-      </svg>
-      } >
+      prevIcon={ <img src={prevButton} alt="anterior" /> }
+      nextIcon={ <img src={nextButton} alt="próxima" /> } >
       <Carousel.Item>
         <div className={styles.img} />
         <Carousel.Caption>
