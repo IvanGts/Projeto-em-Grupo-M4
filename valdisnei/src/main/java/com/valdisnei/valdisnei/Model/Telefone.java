75d6d0db6d0f5b30402510558a9bd1b79e8cdd1c
Victor <victorfaccioli@hotmail.com>
2023-03-19 14:39:34 -0300

    Implementado dados telefone

--- valdisnei/src/main/java/com/valdisnei/valdisnei/Model/Telefone.java
+++ valdisnei/src/main/java/com/valdisnei/valdisnei/Model/Telefone.java
@@ -1,13 +1,16 @@
 package com.valdisnei.valdisnei.Model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-
+@AllArgsConstructor
+@NoArgsConstructor
 public class Telefone {
 
     @Id //Definir primary key
---
+++ valdisnei/src/main/java/com/valdisnei/valdisnei/service/CrudTelefoneService.java
@@ -0,0 +1,41 @@
+package com.valdisnei.valdisnei.service;
+
+
+import com.valdisnei.valdisnei.Bo.TelefoneBo;
+import com.valdisnei.valdisnei.Dto.TelefoneDto;
+import com.valdisnei.valdisnei.Model.Telefone;
+import com.valdisnei.valdisnei.repository.TelefoneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class CrudTelefoneService { // dependencia de classes CrudService
+    @Autowired
+    private TelefoneRepository telefoneRepository;
+    @Autowired
+    private TelefoneBo telefoneBo;
+    public List<Telefone> pegarTodosTelefone(){
+        return telefoneRepository.findAll();
+
+    }
+    public void editarTelefone(TelefoneDto telefoneDto, int id){
+        Optional<Telefone> telefone = telefoneRepository.findById(id);
+        Telefone telefone1 = telefone.orElseThrow();
+        telefoneBo.parseToEntity(telefoneDto, telefone1);
+        telefoneRepository.save(telefone1);
+    }
+
+    public Telefone criarTelefone(TelefoneDto telefoneDto) {
+        Telefone telefone = telefoneBo.parseToEntity(telefoneDto,null);
+        telefoneRepository.save(telefone);
+        return telefone;
+    }
+
+    public void deleteTelefone(int id) {
+        telefoneRepository.deleteById(id);
+    }
+}
