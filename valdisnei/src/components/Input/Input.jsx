413231de9b269e09d2203732c0c6c4d3d2a7ec6c
Jhonny515 <jonatascemporcentojesus@gmail.com>
2023-03-20 20:23:31 -0300

    Adicionada variante de input

--- valdisnei/src/components/Input/Input.jsx
+++ valdisnei/src/components/Input/Input.jsx
@@ -1,8 +1,38 @@
 import styles from './Input.module.css'
+import eyeOff from "../../assets/vectors/eye-off.svg";
+import eye from "../../assets/vectors/baseline-eye.svg";
+import { useState } from 'react';
 
-export default function Input(props) {
+export function Input(props) {
 
     return (
-        <input type={props.tipo} placeholder={props.placeholder} name={props.name} id={props.name} className={styles.TextInput} required={props.required} />
+        <input type={props.tipo} placeholder={props.placeholder} name={props.name} id={props.name} className={styles.TextInput} required={props.required}
+        min={props.min} max={props.max} pattern={props.pattern} style={props.style} />
     )
 }
+
+export function Senha(props) {
+    const [mostraSenha, setMostraSenha] = useState(false)
+
+    return (
+        <div className={styles.BoxSenha}>
+            <input type={mostraSenha?'text':'password'} placeholder={props.placeholder} name={props.name} id={props.name} className={styles.TextInput + " " + styles.Senha} required={props.required}
+            minLength="6" maxLength="12" pattern={props.pattern} style={props.style} />
+            <img src={mostraSenha?eyeOff:eye} 
+            alt={mostraSenha?'Mostrar senha':'Ocultar senha'} 
+            title={mostraSenha?'Mostrar senha':'Ocultar senha'} 
+            className={styles.Eye}
+            onClick={()=> setMostraSenha(!mostraSenha)} />            
+        </div>
+    )
+}
+
+export function Select(props) {
+    return (
+        <select name={props.name} id={props.name} className={styles.TextInput}>
+            {props.children}
+        </select>
+    );
+  }
+
+export default Input;
