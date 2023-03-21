279c0f2dd2ab4520b5abc77db9bb8f5d4c932d42
izabelmafisa <belmarfisa@gmail.com>
2023-03-21 19:09:23 -0300

    criação pagina filme

---
+++ valdisnei/src/pages/Filme/Filme.jsx
@@ -0,0 +1,13 @@
+import Carrossel from "../../components/Carrossel/Carrossel";
+import List from "../../components/Lists/Lists";
+
+export default function Inicio() {
+    return (
+        <>
+        <Carrossel />
+        <List tituloLista={"Filmes mais Assistidos"}/>
+        
+
+        </>
+    )
+}
