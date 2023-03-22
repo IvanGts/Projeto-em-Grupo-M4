950cf98844539e79e3dfac7af5493f6aae7b0311
Jhonny515 <jonatascemporcentojesus@gmail.com>
2023-03-21 21:54:07 -0300

    Criada página de perfil de artista

--- valdisnei/src/App.jsx
+++ valdisnei/src/App.jsx
@@ -2,12 +2,13 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
-import Inicio from './pages/Inicio/Inicio';
+import { Inicio, InicioLogged } from './pages/Inicio/Inicio';
 import Login from './pages/Login/Login'
 import Cadastro from "./pages/Cadastro/Cadastro";
 import Filmes from './pages/Filmes/Filmes'
 import Footer from './components/Footer/Footer';
 import PerfilUsuario from './pages/PerfilUsuario/PerfilUsuario';
+import {PerfilMusico, PerfilAtor} from './pages/PerfilArtista/PerfilArtista'
 
 function App() {
 
@@ -16,11 +17,12 @@ function App() {
       <Router>
         <Header estaLogado={false} neutro={false} />
         <Routes>
-          <Route path="/" element={<Inicio/>}/>
+          <Route path="/" element={<InicioLogged/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/cadastro' element={<Cadastro/>} />
           <Route path='/filmes' element={<Filmes/>} />
           <Route path='/perfil' element={<PerfilUsuario NomeUsuario={"Alguem"}/>}/>
+          <Route path='/pArtista' element={<PerfilAtor/>} />
         </Routes>
         <Footer/>
       </Router>
---
+++ valdisnei/src/pages/PerfilArtista/PerfilArtista.jsx
@@ -0,0 +1,68 @@
+import CardsMusica from "../../components/CardsMusica/CardsMusica";
+import CardsFilme from "../../components/CardsFilme/CardsFilme";
+import NomeMusica from "../../components/NomeMusica/NomeMusica";
+import { List,ListMusicas } from "../../components/Lists/Lists";
+import styles from './PerfilArtista.module.css'
+import Usuario from '../../assets/vectors/usuario.svg'
+
+
+export function PerfilMusico(props){
+    return(
+        <>
+        <div>
+            <div className={styles.Container}>
+            <h2>{props.NomeUsuario}</h2>
+            <img src={Usuario} className={styles.Img}></img>
+            </div>
+            <div className={styles.Container2}>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates sequi eos consectetur in! Beatae vel ex repellat amet cumque expedita eaque sapiente accusantium dignissimos, odit in magnam modi fuga praesentium.</span>
+            <div className={styles.Vazio}></div>
+            </div>
+            
+        </div>
+    
+        <ListMusicas tituloLista={"Músicas do Artista"}>
+            <div className={styles.List}>
+            <NomeMusica></NomeMusica>
+            <NomeMusica></NomeMusica>
+            <NomeMusica></NomeMusica>
+            </div>
+            <div className={styles.List}>
+            <NomeMusica></NomeMusica>
+            <NomeMusica></NomeMusica>
+            <NomeMusica></NomeMusica>
+            </div>
+        </ListMusicas>
+
+        </>
+    )
+}
+
+export default PerfilMusico;
+
+export function PerfilAtor(props){
+    return(
+        <>
+        <div>
+            <div className={styles.Container}>
+            <h2>{props.NomeUsuario}</h2>
+            <img src={Usuario} className={styles.Img}></img>
+            </div>
+            <div className={styles.Container2}>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates sequi eos consectetur in! Beatae vel ex repellat amet cumque expedita eaque sapiente accusantium dignissimos, odit in magnam modi fuga praesentium.</span>
+            <div className={styles.Vazio}></div>
+            </div>
+            
+        </div>
+    
+        <List tituloLista="Filmes que participou">
+            <CardsFilme></CardsFilme>
+            <CardsFilme></CardsFilme>
+            <CardsFilme></CardsFilme>
+            <CardsFilme></CardsFilme>
+            <CardsFilme></CardsFilme>
+        </List>
+
+        </>
+    )
+}
