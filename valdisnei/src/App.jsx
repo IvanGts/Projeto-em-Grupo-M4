ca6dac09b1db88ebb35b93914175766c1860d12d
Jhonny515 <jonatascemporcentojesus@gmail.com>
2023-03-21 21:16:02 -0300

    Importação do Footer para as páginas

--- valdisnei/src/App.jsx
+++ valdisnei/src/App.jsx
@@ -5,19 +5,20 @@ import Header from './components/Header/Header';
 import Inicio from './pages/Inicio/Inicio';
 import Login from './pages/Login/Login'
 import Cadastro from "./pages/Cadastro/Cadastro";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 
   return (
     <div className="App">
       <Router>
-        <Header estaLogado={true} neutro={false} />
+        <Header estaLogado={false} neutro={false} />
         <Routes>
           <Route path="/" element={<Inicio/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/cadastro' element={<Cadastro/>} />
         </Routes>
-        
+        <Footer />
       </Router>
     </div>
   )
